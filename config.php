64e262bce484f9f632a09d2dca69cc90ec418079
Feitseit <markuskleeman@gmail.com>
2026-05-10 17:00:07 +0300

    test

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $db_kasutaja = 'markus';
 $db_salasona = 'markus';
 
 define('ADMIN_USERNAME', 'admin');
-define('ADMIN_PASSWORD_HASH', '$2b$12$0FTDh.QuAUZACF4xGfgkauYlIsXbXXh7oJf.WrwRerGhAGuy89ooS');
+define('ADMIN_PASSWORD_HASH', '$2y$12$xEUDCTa4UHcx3tb/Xo6X9uhf58D6o1z9h5gqauD9Yn8H2XqcCR6hC');
 
 // ühendus andmebaasiga
 $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
